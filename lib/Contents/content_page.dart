bcf73e62fee95701cd6e5372b1f2e5a044889bc4
Azurei0 <mhxc90933@gmail.com>
2022-01-26 23:26:52 +0800

    staged commit

--- lib/Contents/content_page.dart
+++ lib/Contents/content_page.dart
@@ -1,7 +1,16 @@
 //import 'package:badges/badges.dart';
+import 'dart:convert';
+
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:project_test/Contents/content_page2.dart';
 import 'package:project_test/Models/database.dart';
+import 'package:project_test/library/courselib.dart';
+import 'dart:async';
+
+import 'package:shared_preferences/shared_preferences.dart';
 //import 'package:project_test/bookmark_page.dart';
 
 class ContentPage extends StatefulWidget {
@@ -12,48 +21,57 @@ class ContentPage extends StatefulWidget {
 }
 
 class _ContentPageState extends State<ContentPage> {
-
   //List<String> newTitle = nouns.take(5).toList();
   List<String> bookmarked = <String>[];
+  List bookmarkList = [];
+  late SharedPreferences sharedPreferences;
 
   late Database dBContent;
+  List<Database> userCourse = <Database>[];
+
   List docs = [];
-  initialize(){
+  initialize() {
     dBContent = Database();
     dBContent.initialize();
     dBContent.dBContent("ICT").then((value) => {
-      setState((){
-        docs = value;
-      })
-    });
+          setState(() {
+            docs = value;
+          })
+        });
   }
 
   @override
   void initState() {
-    super.initState();
     initialize();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 42, 147, 142),
-      appBar: AppBar(title: const Text("MODULES", style: TextStyle(color: Colors.black,),),
-        backgroundColor: const Color.fromARGB(255, 245, 200, 64),centerTitle: true,
+      appBar: AppBar(
+        title: const Text(
+          "MODULES",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 245, 200, 64),
+        centerTitle: true,
 
         //actions: <Widget>[
-          //Badge(
-            //badgeContent: Text('${bookmarked.length}'),
-            //toAnimate: false,
-            //position: BadgePosition.topStart(),
-            //child: IconButton(
-              //icon: const Icon(Icons.bookmark),
-              //onPressed: () => pushToFavoriteWordsRoute(context),
-            //),
-         // ),
+        //Badge(
+        //badgeContent: Text('${bookmarked.length}'),
+        //toAnimate: false,
+        //position: BadgePosition.topStart(),
+        //child: IconButton(
+        //icon: const Icon(Icons.bookmark),
+        //onPressed: () => pushToFavoriteWordsRoute(context),
+        //),
+        // ),
         //],
-
-        ),
+      ),
       body: body(),
     );
   }
@@ -62,44 +80,47 @@ class _ContentPageState extends State<ContentPage> {
     return ListView.builder(
       itemCount: docs.length,
       itemBuilder: (BuildContext context, int index) {
-
         String word = docs[index]['title'];
         bool newTitle = bookmarked.contains(word);
 
         return Card(
-          margin: const EdgeInsets.all(10),
-          child: ListTile(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => ContentPage2(title:docs[index],dBContent:dBContent)));
-            },
-            contentPadding: const EdgeInsets.only(right: 30, left: 36),
-            title: Text(docs[index]['title'],
-            style: const TextStyle(
-              fontSize: 20
-            ),),
-            trailing: Icon(
-              newTitle ? Icons.bookmark : Icons.bookmark_border,
-              color: newTitle ? Colors.black : null,
-          ),
-            onLongPress: () {
-              setState(() {
+            margin: const EdgeInsets.all(10),
+            child: ListTile(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ContentPage2(
+                            title: docs[index], dBContent: dBContent)));
+              },
+              contentPadding: const EdgeInsets.only(right: 30, left: 36),
+              title: Text(
+                docs[index]['title'],
+                style: const TextStyle(fontSize: 20),
+              ),
+              trailing: Icon(
+                newTitle ? Icons.bookmark : Icons.bookmark_border,
+                color: newTitle ? Colors.black : null,
+              ),
+              onLongPress: () async {
                 if (newTitle) {
                   bookmarked.remove(word);
+                  print(docs[index]);
                 } else {
                   bookmarked.add(word);
                 }
-                //Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => BookmarkPage(bookmarkItems: bookmarked,)));
-              });
-            },
-          )
-        );
+                setState(() {
+                  //Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => BookmarkPage(bookmarkItems: bookmarked,)));
+                });
+              },
+            ));
       },
     );
   }
   //Future pushToFavoriteWordsRoute(BuildContext context) {
-    //return Navigator.of(context).push(
-        //MaterialPageRoute(builder:
-            //(BuildContext context) => BookmarkPage(bookmarkItems: bookmarked,))
-    //);
+  //return Navigator.of(context).push(
+  //MaterialPageRoute(builder:
+  //(BuildContext context) => BookmarkPage(bookmarkItems: bookmarked,))
+  //);
   //}
 }
